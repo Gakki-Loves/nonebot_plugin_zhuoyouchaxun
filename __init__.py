9117e4abcf5351e596507344b31b3969260e55c3
Gakkilove <739150373@qq.com>
2023-01-14 11:18:50 +0800

    updata time

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-
 #'''
-# Author: Gakkilove 739150373@qq.com
-# Date: 2023-01-11 20:57:18
-# LastEditors: Gakkilove 739150373@qq.com
-# LastEditTime: 2023-01-13 21:28:46
-# FilePath: \undefinedd:\Github\LihuaBot\nb2\LihuaBot\src\plugins\nonebot_plugin_zhuoyouchaxun\__init__.py
-# Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-# '''
+#Author: Gakkilove 739150373@qq.com
+#Date: 2023-01-14 11:02:34
+#LastEditors: Gakkilove 739150373@qq.com
+#LastEditTime: 2023-01-14 11:09:51
+#FilePath: \nonebot_plugin_zhuoyouchaxun\get_data.py
+#Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+#'''
+
 import asyncio
 from nonebot.plugin import on_keyword,on_regex
 from nonebot.adapters.onebot.v11 import Bot, Event
