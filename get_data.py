c6764d3fc515492056d1c0c712b74013d4beb656
Gakkilove <739150373@qq.com>
2023-01-14 11:10:25 +0800

    update

--- get_data.py
+++ get_data.py
@@ -1,10 +1,12 @@
-#"""
+#'''
 #Author: Gakkilove 739150373@qq.com
-#Date: 2023-01-12 21:06:03
+#Date: 2023-01-14 11:02:34
 #LastEditors: Gakkilove 739150373@qq.com
-#LastEditTime: 2023-01-13 23:14:10
-#FilePath: \undefinedd:\Github\LihuaBot\nb2\LihuaBot\src\plugins\nonebot_plugin_zhuoyouchaxun\get_data.py
-#"""
+#LastEditTime: 2023-01-14 11:09:51
+#FilePath: \nonebot_plugin_zhuoyouchaxun\get_data.py
+#Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+#'''
+
 
 
 import os
