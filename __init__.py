85cf72d2399befd709e61b71b777f8f1f0780570
RayLee9527 <38999345+RayLee9527@users.noreply.github.com>
2023-01-29 21:22:42 +0800

    Update __init__.py
    删除了权限控制的部分

--- __init__.py
+++ __init__.py
@@ -387,22 +387,6 @@ async def _():
 
 # -----------------------------------------------------
 
-# -----------------------权限控制-----------------------
-# NoneBot中定义一个群员权限等级列表
-members_level = {
-    123456789: 1, # QQ号为123456789，权限等级为1
-    987654321: 2, # QQ号为987654321，权限等级为2
-}
-from nonebot import on_command
-
-#指令权限控制
-@on_command('command', permission=1)  # 只有权限等级为1的群员才能执行该命令
-async def _(session):
-    if members_level.get(session.event.user_id, 0) < 1: 
-        await session.send('你没有权限执行此命令')
-        return
-# -----------------------------------------------------
-
 # -----------------------权限控制-管理员单独版----------------------
 from nonebot import on_command, permission
 
