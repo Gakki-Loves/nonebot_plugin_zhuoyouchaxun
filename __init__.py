a82798e48c45a2cf23423b22862b078da5574065
Gakkilove <739150373@qq.com>
2023-03-06 21:46:10 +0800

    增加了发车时广播的间隔

--- __init__.py
+++ __init__.py
@@ -28,6 +28,7 @@ from nonebot.adapters.onebot.v11 import GROUP_ADMIN, GROUP_OWNER
 from nonebot_plugin_txt2img import Txt2Img
 import sqlite3
 from pathlib import Path
+import asyncio
 import os
 import json
 from nonebot.typing import T_State
@@ -470,6 +471,7 @@ async def _(bot: Bot,state:T_State,event: GroupMessageEvent,deadline: str = ArgP
                 if cmd_broadcast:
                     onBroadCastGroupNum+=1
                     await bot.send_group_msg(group_id=group["group_id"], message=(content+"\n截止时间："+deadline+"\n这条是多群广播信息，第二轮测试期间，发车信息被多群广播只有在梨花的图书馆（群号：177053575）才可以使用哦！"))
+                    await asyncio.sleep(1)
             await run_car.finish(f"梨花一共加入了{allGroupNum}个群，已经帮您广播转发到了{onBroadCastGroupNum}个群，其余群关闭了接收广播功能~")
         elif cmd_broad_cast == False:
             await run_car.finish("多群广播功能没有开启呦~梨花已经帮你记录到车库啦！")
